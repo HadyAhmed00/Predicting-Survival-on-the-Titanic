cced60deb28270ec987f61c33371051fcb3d9e3c
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2023-04-15 15:02:00 +0200

    Update Titanic Scrept.R

--- Titanic Scrept.R
+++ Titanic Scrept.R
@@ -1,8 +1,8 @@
-setwd("C:/Users/Hady Ahmed (Main)/Documents/GitHub/Predicting-Survival-on-the-Titanic/Data")
-saveing_dir = "C:/Users/Hady Ahmed (Main)/Documents/GitHub/Predicting-Survival-on-the-Titanic/Addetional files"
-test_data <- read.table("test.csv", sep=",", header=TRUE)
-train_data <- read.table("train.csv", sep=",", header=TRUE) 
-real_test_labes <- read.table("submission.csv", sep=",", header=TRUE)
+setwd("C:/Users/Hady Ahmed (Main)/Documents/GitHub/Predicting-Survival-on-the-Titanic")
+saveing_dir = "C:/Users/Hady Ahmed (Main)/Documents/GitHub/Predicting-Survival-on-the-Titanic/Addetional files/"
+test_data <- read.table("Data/test.csv", sep=",", header=TRUE)
+train_data <- read.table("Data/train.csv", sep=",", header=TRUE) 
+real_test_labes <- read.table("Addetional files/100 supmation.csv", sep=",", header=TRUE)
 ##############################################################
 # Some Data Exploratin and visulization
 #############################################################
@@ -13,7 +13,6 @@ survival_count <- table(train_data$Survived)
 barplot(survival_count, main="Passenger Survival",
         xlab="Survived", ylab="Count", col=c("#FF5733", "#5F9EA0"), 
         legend=c("No", "Yes"))
-
 
 # Create a histogram of passenger age
 hist(train_data$Age, main="Passenger Age", xlab="Age", ylab="Count", col="lightblue")
@@ -34,7 +33,6 @@ class_survival_count <- table(train_data$Pclass, train_data$Survived)
 # Create a stacked bar plot of passenger class and survival
 barplot(class_survival_count, main="Passenger Class and Survival",
         xlab="Class", ylab="Count", col=c("#FF5733", "#5F9EA0"), legend=c("No", "Yes"), beside=TRUE)
-
 
 #install.packages("ggplot2")
 # install.packages("colorspace")
@@ -101,18 +99,52 @@ preprocessing <- function(data) {
   X <- X[ , !(names(X) %in% c("Sex"))]
   
   # Look for the null values in the data
-  print(colSums(is.na(X)))
+  #print(colSums(is.na(X)))
   
   # Fill the null values in the 'Age' column by the "mean" value
   X$Age[is.na(X$Age)] <- mean(X$Age, na.rm = TRUE)
   X$Fare[is.na(X$Fare)] <- mean(X$Fare, na.rm = TRUE)
   
-  print("After Null Handle")
-  print(colSums(is.na(X)))
+  #print("After Null Handle")
+  #print(colSums(is.na(X)))
   
   return (X)
 }
-
+##############################################################
+# Apply the preporcessing on the data and split it to train and test
+#############################################################
+evaluation<- function(model, model_name){
+  # apply the same preporccsing in the test data
+  test_data_for_predection <- preprocessing(test_data) 
+  # Predicting the Data of Passengers
+  if (model_name =="logistic regression"||model_name =="Random Forest") {
+    train_predictions <- ifelse(predict(model, type = "response", newdata = X_train) > 0.5, 1, 0)
+    test_predictions<-ifelse(predict(model, type = "response", newdata = X_test) > 0.5, 1, 0)
+    test_prediction <- ifelse(predict(model, type = "response", newdata = test_data_for_predection) > 0.5, 1, 0)
+  }else{
+    train_predictions <- predict(model, newdata = X_train) 
+    test_predictions<-predict(model, newdata = X_test) 
+    test_prediction <-predict(model, newdata = test_data_for_predection) 
+  }
+  
+  # Comparing the The model results with the true lables 
+  train_accuracy <- mean(train_predictions == X_train$Survived)
+  test_accuracy <- mean(test_predictions == X_test$Survived)
+  kaggel_test_accurcy <-mean(test_prediction == real_test_labes$Survived)
+  
+  # Extracting the Data as csv file after prediction
+  test_data$Survived <- test_prediction
+  submation <- test_data[ , !(names(test_data) %in% c("Pclass","Sex","Age","SibSp",
+                                                      "Parch","Fare","take.off",
+                                                      "female","male","Name", 
+                                                      "Ticket", "Cabin",
+                                                      "take-off"))]
+  write.csv(submation,paste("Addetional files/Sub_Model_",model_name,".csv"),row.names = FALSE)
+  # Printing
+  print(paste("Train accuracy for ",model_name,":", round(train_accuracy, 3)))
+  print(paste("Test accuracy for ",model_name,":", round(test_accuracy, 3)))
+  print(paste("The kaggel Test accuracy is : ",kaggel_test_accurcy))
+}
 ##############################################################
 # Apply the preporcessing on the data and split it to train and test
 #############################################################
@@ -130,59 +162,18 @@ X_test <- X[-train_index, ]
 # install.packages("e1071")
 library(e1071)
 model_Naive_Bayes <- naiveBayes(Survived ~ ., data = X_train)
-
-# Evaluate the model on the train set
-train_predictions <- predict(model_Naive_Bayes, newdata = X_train) 
-train_accuracy <- mean(train_predictions == X_train$Survived)
-print(paste("Train accuracy for NaiveBayes:", round(train_accuracy, 3)))
-
-test_predictions <- predict(model_Naive_Bayes, newdata = X_test)
-test_accuracy <- mean(test_predictions == X_test$Survived)
-print(paste("Test accuracy for NaiveBayes :", round(test_accuracy, 3)))
-
-##############################################################
-# Evaluating The Naive Bayes Model
-#############################################################
-test_data_for_predection <- preprocessing(test_data)
-# Predicting the Data of Passengers
-test_prediction <- predict(model_Naive_Bayes, test_data_for_predection)
-# Extracting the Data as csv file after prediction
-test_data$Survived <- test_prediction
-submation <- test_data[ , !(names(test_data) %in% c("Pclass","Sex","Age","SibSp",
-                                                    "Parch","Fare","take.off",
-                                                    "female","male","Name", 
-                                                    "Ticket", "Cabin",
-                                                    "take-off"))]
-write.csv(submation,"submation.csv",row.names = FALSE) # get 81.7% in kaggel compitaion
+# Evaluate the model
+evaluation(model = model_Naive_Bayes, "Naive Bayes")
 
 ##############################################################
 # Create The logistic regression Model and fit in the data
 #############################################################
 model_Log_reg <- glm(Survived ~ ., data = X_train, family = binomial)
-
-# Evaluate the model on the train set
-train_predictions <- ifelse(predict(model_Log_reg, type = "response", newdata = X_train) > 0.5, 1, 0)
-train_accuracy <- mean(train_predictions == X_train$Survived)
-print(paste("Train accuracy logistic regression:", round(train_accuracy, 3)))
-
-# Evaluate the model on the test set
-test_predictions <- ifelse(predict(model_Log_reg, type = "response", newdata = X_test) > 0.5, 1, 0)
-test_accuracy <- mean(test_predictions == X_test$Survived)
-print(paste("Test accuracy logistic regression:", round(test_accuracy, 3)))
-
+# Evaluate the model
+evaluation(model = model_Log_reg, "logistic regression")
 ##############################################################
-# Evaluating The logistic regression Model
-#############################################################
-test_data_for_predection <- preprocessing(test_data)
-# Predicting the Data of Passengers
-test_prediction <-ifelse(predict(model_Log_reg, type = "response", newdata = test_data_for_predection) > 0.9, 1, 0)
-# Extracting the Data as csv file after prediction
-#res <- mean(test_prediction == real_test_labes$Survived)
-#res
-test_data$Survived <- test_prediction
-submation <- test_data[ , !(names(test_data) %in% c("Pclass","Sex","Age","SibSp",
-                                                    "Parch","Fare","take.off",
-                                                    "female","male","Name", 
-                                                    "Ticket", "Cabin",
-                                                    "take-off"))]
-write.csv(submation,"submation.csv",row.names = FALSE) # get 70.3% in kaggel compitaion
+# Creating Random Forest Model
+##############################################################
+library(randomForest)
+model_randomForest <- randomForest(Survived ~ ., data = X_train)
+evaluation(model = model_randomForest,"Random Forest")
